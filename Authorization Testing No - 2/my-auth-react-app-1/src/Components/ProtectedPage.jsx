dfcd4b215e4cd101ae8f04ca22818673bcbd6bca
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-09-20 18:23:39 +0530

    avlothaan mudichi vitanga ponga

--- Authorization Testing No - 2/my-auth-react-app-1/src/Components/ProtectedPage.jsx
+++ Authorization Testing No - 2/my-auth-react-app-1/src/Components/ProtectedPage.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 export default function ProtectedPage()
 {
-    return <>
+    const [ isAuthenticated, setIsAuthenticated] = useState(false)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+
+        const token = localStorage.getItem('authToken')
+
+        const expectedToken = "sibikrishnaistobusyrightnow"
+
+        if(token && token === expectedToken) {
+            setIsAuthenticated(true)
+        } else {
+
+            navigate('/FinalLogin')
+
+        }
+
+    },[navigate])
+
+    return isAuthenticated ?  (
         <h1>this is Protected - Page</h1>
-    </>
+    ) : null
 }
