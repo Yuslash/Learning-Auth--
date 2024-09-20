029eafff30944322228b7774b77ca418ca09d9fc
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-09-20 18:11:47 +0530

    adding two routes login, protecteed page and finallogin for token

--- Authorization Testing No - 2/my-auth-react-app-1/src/App.jsx
+++ Authorization Testing No - 2/my-auth-react-app-1/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 import Signup from "./Components/Signup"
 import LoginPage from "./Components/LoginPage"
+import ProtectedPage from './Components/ProtectedPage'
+import FinalLogin from "./Components/FinalLogin"
 
 export default function App() {
 
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Protected" element={<ProtectedPage />} />
+        <Route path="/FinalLogin" element={<FinalLogin />} />
       </Routes>
     </Router>
   </>
