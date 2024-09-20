1cf75e2e0e500a2b211aa13a7ff16325173ac6f0
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-09-20 18:14:25 +0530

    created an final login page for to add token in localstorage

---
+++ Authorization Testing No - 2/my-auth-react-app-1/src/Components/FinalLogin.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function FinalLogin()
+{
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const handleLogin = () =>
+    {
+         const userExist = Object.values(localStorage).some((storedUser) => {
+            try {
+
+                const user = JSON.parse(storedUser)
+
+                return user.username === username && user.password === password
+
+            } catch(error) {
+
+                alert(`Error parsing to localStorage ${error}`)
+
+            }
+
+        })
+
+        if (userExist) {
+
+            const token = "sibikrishnaistobusyrightnow"
+
+            localStorage.setItem('authToken', token)
+
+            alert(`Success! you have been login ${username}`)
+
+            navigate('/Protected')
+
+        } else {
+            alert(`Invalid Username and password`)
+        }
+
+    }
+
+    return <div className="flex flex-col gap-2">
+
+        <h1>Final-Login Page</h1>
+
+        <input
+        className="p-4 text-black"
+        type="text"
+        placeholder="Enter your Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+         />
+
+        <input
+        className="p-4 text-black"
+        type="password"
+        placeholder="Enter your Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} 
+         />
+
+         <button onClick={handleLogin} className=" p-4 bg-lime-300 text-black font-semibold text-lg">Submit - FinalLogin</button>
+
+    </div>
+}
