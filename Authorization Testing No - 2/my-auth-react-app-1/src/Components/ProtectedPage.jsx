0a34015cdf6fca1ecc5697c23e0100bd53df1393
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-09-21 15:33:00 +0530

    adding there is json parsing error invlaid fromat like so fixed that

--- Authorization Testing No - 2/my-auth-react-app-1/src/Components/ProtectedPage.jsx
+++ Authorization Testing No - 2/my-auth-react-app-1/src/Components/ProtectedPage.jsx
@@ -3,26 +3,82 @@ import { useNavigate } from "react-router-dom"
 
 export default function ProtectedPage()
 {
-    const [ isAuthenticated, setIsAuthenticated] = useState(false)
+    const [ isAuthenticated, setIsAuthenticated] = useState(true)
+    const [ username, setUsername ] = useState("")
     const navigate = useNavigate()
 
     useEffect(() => {
 
-        const token = localStorage.getItem('authToken')
+        const storedAuthToken = localStorage.getItem('authToken')
 
-        const expectedToken = "sibikrishnaistobusyrightnow"
+        if(storedAuthToken) {
+            try {
+                
+                const parsedAuthToken = JSON.parse(storedAuthToken)
 
-        if(token && token === expectedToken) {
-            setIsAuthenticated(true)
-        } else {
+                const token = parsedAuthToken.token
 
-            navigate('/Signup')
+                const expectedToken = import.meta.env.VITE_APP_TOKEN
 
+                if(token && token === expectedToken) 
+                {
+                    setIsAuthenticated(true)
+
+                } else {
+
+                    navigate('/Signup')
+
+                }
+
+            } catch (error) {
+
+                console.log(`Invalid json in authToken ${error}`);
+
+            }
         }
 
     },[navigate])
 
+
+    useEffect(() => {
+
+        let some = []
+
+        Object.values(localStorage).some((storedUser) => {
+
+            try {
+
+                const isJson = storedUser.startsWith('{') && storedUser.endsWith('}')
+
+                if(isJson) {
+
+                    const user = JSON.parse(storedUser)
+
+                    if (user && user.username) {
+
+                        some.push(user.username)
+
+                    }
+
+                }
+
+            } catch (error) {
+
+                console.log(`Invalid Json ERROR${error}`)
+
+            }
+
+        })
+
+        setUsername(some)
+
+    }, [])
+    
+
     return isAuthenticated ?  (
-        <h1>this is Protected - Page</h1>
+        <>
+            <h1 className="font-bold text-3xl">Welcome {username}</h1>
+            <h1 className=" font-semibold text-2xl">this is Protected - Page</h1>
+        </>
     ) : null
 }
